fcd73cdde785458e0d5b5c7315018b1b5a728af0
Gonzaga <zaga.silva@gmail.com>
2011-10-13 19:22:30 +0100

    Teste

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SmartSeller::Application.config.secret_token = 'c172e314a27bdc5bbd05765cf7d7c1df33a61721bb42720cf98623190f337ae2dc8a756ccfadd7e0dba71e8a332d4a02b2b845c42084712c4ab44ff405e57d07'
